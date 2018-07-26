e14506251c1828899b409f457e5c0b7bd5db01d5
George Liu <eva2000@centminmod.com>
2018-07-27 09:02:48 +1000

    gcc 8.2.0 stable + binutils 2.31

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 # https://gist.github.com/centminmod/f825b26676eab0240d3049d2e7d1c688
 # http://wiki.osdev.org/GCC_Cross-Compiler#Binutils
 ################################################
-VER='0.5'
+VER='0.7'
 DT=$(date +"%d%m%y-%H%M%S")
 DIR_TMP='/svr-setup'
 
@@ -36,7 +36,7 @@ GCC_GOLD='y'
 # https://gcc.gnu.org/install/build.html
 GCC_PGO='n'
 BOOTCFLAGS='y'
-BINUTILS_VER='2.30'
+BINUTILS_VER='2.31'
 
 # GCC Downloads
 GMP_FILE='gmp-6.1.0.tar.bz2'
@@ -50,8 +50,8 @@ CCACHE='y'
 CENTMINLOGDIR='/root/centminlogs'
 #GCC_SNAPSHOTSEVEN='http://www.netgull.com/gcc/snapshots/LATEST-7/'
 GCC_SNAPSHOTSEVEN='http://www.netgull.com/gcc/releases/gcc-7.3.0/'
-GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/snapshots/LATEST-8/'
-#GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/snapshots/8.0.1-RC-20180425/'
+#GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/snapshots/LATEST-8/'
+GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/releases/gcc-8.2.0/'
 #GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/releases/gcc-8.1.0/'
 GCC_COMPILEOPTS='--enable-bootstrap --enable-plugin --with-gcc-major-version-only --enable-shared --disable-nls --enable-threads=posix --enable-checking=release --with-system-zlib --enable-__cxa_atexit --disable-install-libiberty --disable-libunwind-exceptions --enable-gnu-unique-object --enable-linker-build-id --with-linker-hash-style=gnu --enable-languages=c,c++ --enable-initfini-array --disable-libgcj --enable-gnu-indirect-function --with-tune=generic --build=x86_64-redhat-linux'
 SCRIPT_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
@@ -495,7 +495,7 @@ install_gcc() {
         ../configure --prefix="$GCC_PREFIX" --disable-multilib $GCC_COMPILEOPTS
     elif [[ "$GCC_SVN" = [yY] && "$GCCSVN_VER" -eq '8' ]]; then
         GCC_SYMLINK='/opt/gcc8'
-        GCCFPM_VER='8.1.1'
+        GCCFPM_VER='8.2.0'
         downloadtar_name=$(curl -4s $GCC_SNAPSHOTEIGHT | grep -o '<a .*href=.*>' | grep -v '.sig' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | awk -F "/" '/tar.xz/ {print $2}')
         downloadtar_dirname=$(echo "$downloadtar_name" | sed -e 's|.tar.xz||')
         rm -rf "${downloadtar_dirname}"
