763fe5dac455e48a463e2c2a09562c4dc7e9083d
George Liu <eva2000@centminmod.com>
2019-12-26 00:49:25 +1000

    gcc 10 prep

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 # https://gist.github.com/centminmod/f825b26676eab0240d3049d2e7d1c688
 # http://wiki.osdev.org/GCC_Cross-Compiler#Binutils
 ################################################
-VER='1.3'
+VER='1.4'
 DT=$(date +"%d%m%y-%H%M%S")
 DIR_TMP='/svr-setup'
 
@@ -40,9 +40,9 @@ BINUTILS_VER='2.33.1'
 
 # GCC Downloads
 GMP_FILE='gmp-6.1.2.tar.bz2'
-ISL_FILE='isl-0.22.tar.bz2'
+ISL_FILE='isl-0.18.tar.bz2'
 MPC_FILE='mpc-1.1.0.tar.gz'
-MPFR_FILE='mpfr-4.0.2.tar.bz2'
+MPFR_FILE='mpfr-3.1.4.tar.bz2'
 
 CLANG_FOUR='n'
 OPT_LEVEL=-O2
@@ -54,8 +54,9 @@ GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/snapshots/LATEST-8/'
 #GCC_SNAPSHOTEIGHT='http://www.netgull.com/gcc/releases/gcc-8.2.0/'
 #GCC_SNAPSHOTNINE='http://www.netgull.com/gcc/snapshots/LATEST-9/'
 #GCC_SNAPSHOTNINE='http://mirror.koddos.net/gcc/snapshots/LATEST-9/'
-# GCC_SNAPSHOTNINE='http://mirrors.concertpass.com/gcc/snapshots/LATEST-9/'
-GCC_SNAPSHOTNINE='http://mirrors.concertpass.com/gcc/snapshots/9-20191207/'
+GCC_SNAPSHOTNINE='https://bigsearcher.com/mirrors/gcc/snapshots/LATEST-9/'
+#GCC_SNAPSHOTNINE='http://mirrors.concertpass.com/gcc/snapshots/9-20191207/'
+GCC_SNAPSHOTTEN='https://bigsearcher.com/mirrors/gcc/snapshots/LATEST-10/'
 GCC_COMPILEOPTS='--enable-bootstrap --enable-plugin --with-gcc-major-version-only --enable-shared --disable-nls --enable-threads=posix --enable-checking=release --with-system-zlib --enable-__cxa_atexit --disable-install-libiberty --disable-libunwind-exceptions --enable-gnu-unique-object --enable-linker-build-id --with-linker-hash-style=gnu --enable-languages=c,c++ --enable-initfini-array --disable-libgcj --enable-gnu-indirect-function --with-tune=generic --build=x86_64-redhat-linux'
 SCRIPT_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 ################################################
@@ -299,6 +300,11 @@ binutils_install() {
     elif [[ "$GCC_SVN" = [yY] && "$GCCSVN_VER" -eq '9' ]]; then
         GCC_SYMLINK='/opt/gcc9'
         downloadtar_name=$(curl -4s $GCC_SNAPSHOTNINE | grep -o '<a .*href=.*>' | grep -v '.sig' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' -e 's|\.\.\/||g' -e 's|\.\/||g'| awk '/tar.xz/ {print $1}')
+        downloadtar_dirname=$(echo "$downloadtar_name" | sed -e 's|.tar.xz||')
+        GCC_PREFIX="/opt/${downloadtar_dirname}"
+    elif [[ "$GCC_SVN" = [yY] && "$GCCSVN_VER" -eq '10' ]]; then
+        GCC_SYMLINK='/opt/gcc10'
+        downloadtar_name=$(curl -4s $GCC_SNAPSHOTTEN | grep -o '<a .*href=.*>' | grep -v '.sig' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' -e 's|\.\.\/||g' -e 's|\.\/||g'| awk '/tar.xz/ {print $1}')
         downloadtar_dirname=$(echo "$downloadtar_name" | sed -e 's|.tar.xz||')
         GCC_PREFIX="/opt/${downloadtar_dirname}"
     fi
@@ -552,6 +558,34 @@ install_gcc() {
         rm -rf "${downloadtar_name}"
         echo "wget -4 "$GCC_SNAPSHOTNINE/${downloadtar_name}""
         wget -4 "$GCC_SNAPSHOTNINE/${downloadtar_name}"
+        echo "tar xf ${downloadtar_name}"
+        tar xf "${downloadtar_name}"
+        cd "$downloadtar_dirname"
+        if [[ "$GCC_DOWNLOADPREREQ" = [yY] ]]; then
+            ./contrib/download_prerequisites
+        else
+            download_prereq
+        fi
+        echo "mkdir -p test"
+        mkdir -p test
+        echo "cd ${DIR}${downloadtar_dirname}/test" | tee "${SCRIPT_DIR}/gcc${GCCSVN_VER}${PGOTAG}-fpm-cmd"
+        cd test
+        GCC_PREFIX="/opt/${downloadtar_dirname}"
+        if [[ "$CCACHE" != [yY] ]]; then
+            export CC="gcc"
+            export CXX="g++"
+        fi
+        echo "../configure --prefix=$GCC_PREFIX --disable-multilib $GCC_COMPILEOPTS"
+        ../configure --prefix="$GCC_PREFIX" --disable-multilib $GCC_COMPILEOPTS
+    elif [[ "$GCC_SVN" = [yY] && "$GCCSVN_VER" -eq '10' ]]; then
+        GCC_SYMLINK='/opt/gcc10'
+        GCCFPM_VER='10.0.0'
+        downloadtar_name=$(curl -4s $GCC_SNAPSHOTTEN | grep -o '<a .*href=.*>' | grep -v '.sig' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' -e 's|\.\.\/||g' -e 's|\.\/||g'| awk '/tar.xz/ {print $1}')
+        downloadtar_dirname=$(echo "$downloadtar_name" | sed -e 's|.tar.xz||')
+        rm -rf "${downloadtar_dirname}"
+        rm -rf "${downloadtar_name}"
+        echo "wget -4 "$GCC_SNAPSHOTTEN/${downloadtar_name}""
+        wget -4 "$GCC_SNAPSHOTTEN/${downloadtar_name}"
         echo "tar xf ${downloadtar_name}"
         tar xf "${downloadtar_name}"
         cd "$downloadtar_dirname"
@@ -833,6 +867,21 @@ case "$1" in
             echo "Total Binutils + GCC Install Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/tools-binutils-install${PGOTAG}_${DT}.log"
             tail -2 "${CENTMINLOGDIR}/tools-binutils-install${PGOTAG}_${DT}.log"
         ;;
+    binutils10 )
+        GCCSVN_VER='10'
+            starttime=$(TZ=UTC date +%s.%N)
+        {
+            fpm_install
+            binutils_install
+            tidyup
+            # postfixsetup
+        } 2>&1 | tee "${CENTMINLOGDIR}/tools-binutils-install${PGOTAG}_${DT}.log"
+            endtime=$(TZ=UTC date +%s.%N)
+            INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+            echo "" >> "${CENTMINLOGDIR}/tools-binutils-install${PGOTAG}_${DT}.log"
+            echo "Total Binutils + GCC Install Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/tools-binutils-install${PGOTAG}_${DT}.log"
+            tail -2 "${CENTMINLOGDIR}/tools-binutils-install${PGOTAG}_${DT}.log"
+        ;;
     install7 )
         GCCSVN_VER='7'
             starttime=$(TZ=UTC date +%s.%N)
@@ -867,6 +916,22 @@ case "$1" in
         ;;
     install9 )
         GCCSVN_VER='9'
+            starttime=$(TZ=UTC date +%s.%N)
+        {
+            fpm_install
+            binutils_install
+            install_gcc
+            tidyup
+            # postfixsetup
+        } 2>&1 | tee "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            endtime=$(TZ=UTC date +%s.%N)
+            INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+            echo "" >> "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            echo "Total Binutils + GCC Install Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            tail -2 "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+        ;;
+    install10 )
+        GCCSVN_VER='10'
             starttime=$(TZ=UTC date +%s.%N)
         {
             fpm_install
@@ -919,6 +984,24 @@ case "$1" in
         ;;
     installpgo9 )
         GCCSVN_VER='9'
+        GCC_PGO='y'
+        BOOTCFLAGS='y'
+            starttime=$(TZ=UTC date +%s.%N)
+        {
+            fpm_install
+            binutils_install
+            install_gcc
+            tidyup
+            # postfixsetup
+        } 2>&1 | tee "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            endtime=$(TZ=UTC date +%s.%N)
+            INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+            echo "" >> "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            echo "Total Binutils + GCC Install Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            tail -2 "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+        ;;
+    installpgo10 )
+        GCCSVN_VER='10'
         GCC_PGO='y'
         BOOTCFLAGS='y'
             starttime=$(TZ=UTC date +%s.%N)
@@ -994,6 +1077,21 @@ case "$1" in
             echo "Total GCC Install Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
             tail -2 "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
         ;;
+    installgcc10 )
+        GCCSVN_VER='10'
+            starttime=$(TZ=UTC date +%s.%N)
+        {
+            fpm_install
+            install_gcc
+            tidyup
+            # postfixsetup
+        } 2>&1 | tee "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            endtime=$(TZ=UTC date +%s.%N)
+            INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+            echo "" >> "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            echo "Total GCC Install Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            tail -2 "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+        ;;
     installpgogcc7 )
         GCCSVN_VER='7'
         GCC_PGO='y'
@@ -1045,10 +1143,27 @@ case "$1" in
             echo "Total GCC Install Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
             tail -2 "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
         ;;
+    installpgogcc10 )
+        GCCSVN_VER='10'
+        GCC_PGO='y'
+        BOOTCFLAGS='y'
+            starttime=$(TZ=UTC date +%s.%N)
+        {
+            fpm_install
+            install_gcc
+            tidyup
+            # postfixsetup
+        } 2>&1 | tee "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            endtime=$(TZ=UTC date +%s.%N)
+            INSTALLTIME=$(echo "scale=2;$endtime - $starttime"|bc )
+            echo "" >> "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            echo "Total GCC Install Time: $INSTALLTIME seconds" >> "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+            tail -2 "${CENTMINLOGDIR}/tools-gcc-install${PGOTAG}_${DT}.log"
+        ;;
     * )
         echo
         echo "Usage:"
         echo
-        echo "$0 {install|install7|install8|install9|installpgo7|installpgo8|installpgo9|installgcc|installgcc7|installgcc8|installgcc9|installpgogcc7|installpgogcc8|installpgogcc9|binutils7|binutils8|binutils9}"
+        echo "$0 {install|install7|install8|install9|install10|installpgo7|installpgo8|installpgo9|installpgo10|installgcc|installgcc7|installgcc8|installgcc9|installgcc10|installpgogcc7|installpgogcc8|installpgogcc9|installpgogcc10|binutils7|binutils8|binutils9|binutils10}"
         ;;
 esac
